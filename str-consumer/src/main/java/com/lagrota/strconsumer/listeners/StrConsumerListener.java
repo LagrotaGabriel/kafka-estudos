cf794238a466d1d9f64b9707cfda10d263227065
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2024-11-20 14:56:14 -0300

    feat: Consumer configurado por partições

--- str-consumer/src/main/java/com/lagrota/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/lagrota/strconsumer/listeners/StrConsumerListener.java
@@ -2,18 +2,27 @@ package com.lagrota.strconsumer.listeners;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+                    @TopicPartition(topic = "str-topic", partitions = {"0"})
+            },
+            containerFactory = "strContainerFactory")
     public void create(String message) {
         log.info("Create ::: Mensagem recebida: {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+                    @TopicPartition(topic = "str-topic", partitions = {"1"})
+            },
+            containerFactory = "strContainerFactory")
     public void log(String message) {
         log.info("Log ::: Mensagem recebida: {}", message);
     }
